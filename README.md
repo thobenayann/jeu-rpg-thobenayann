0bb4890f1738d68355e2e0335fa21cf5b5b9798a
thobenayann <63640040+thobenayann@users.noreply.github.com>
2021-03-08 17:51:26 +0100

    add-gameplay-elements

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # A simple RPG game using only HTML / CSS / Javascript
 
 to use it :
+
 - copy / past index.html directly in your browser
 - in terminal : google-chrome index.html
 
--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 var app = {
+    NB_ROWS : 4,
+    NB_CELLS: 6,
     // Création de l'objet player qui aura plusieurs propriétés
     // relatives à sa position
     player: {
@@ -13,8 +15,51 @@ var app = {
         y: 3
     },
 
+    // déclaration de la propriété qui va créer notre plateau de jeu
+    boardNode: null,
+
     init: function () {
       console.log('init !');
+      app.boardNode = document.getElementById('board');
+      app.drawBoard();
+    },
+
+    drawBoard: function () {
+
+        // NB_ROWS et NB_CELLS dans les propriétés de app
+        // Ici on doit créer 2 opérations imbriquées
+        // Pour obtenir app.NB_ROWS(nombre de rows) x app.NB_CELLS(nombre de cellules)
+        for (let indexRow = 0; indexRow < app.NB_ROWS; indexRow++) {
+
+            const rowNode = document.createElement('div');
+            rowNode.classList.add('row');
+
+            app.boardNode.appendChild(rowNode);
+
+            for (let indexCell = 0; indexCell < app.NB_CELLS; indexCell++) {
+
+                const cellNode = document.createElement('div');
+                cellNode.classList.add('cell');
+                rowNode.appendChild(cellNode);
+
+                // Tester les coordonnées de la cellule
+                // Si les coordonnées de la cellule sont égales à app.targetCell x et y
+                if (indexCell === app.targetCell.x && indexRow === app.targetCell.y) {
+                    // On ajoute la classe target-cell
+                    cellNode.classList.add('target-cell');
+                }
+
+                // Tester les coordonnées de la cellule
+                // Si les coordonnées de la cellule sont égales à app.player x et y
+                if (indexCell === app.player.x && indexRow === app.player.y) {
+                    // On ajoute une div avec la classe player dans la cellule
+                    const playerNode = document.createElement('div');
+                    playerNode.classList.add('player');
+                    cellNode.appendChild(playerNode);
+                }
+            }
+        }
+
     }
   };
   
